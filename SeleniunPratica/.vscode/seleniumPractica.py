610b7616fe832f50bf98389bc1b50a71c8095e57
Rafael Eduardo <remendezp@televisa.com.mx>
2020-10-10 20:23:27 -0500

    Rescate 5 Tiwtters realizado
    Poro medio de selenium se puede consultar los 5 twitters, para ello es
    necesario hacer un scrool y que se agrupen los nuevos tiwtters en la
    parte visual del portal. Se le incorporara este método a la otra version
    que rescata todos los elementos "articul" para poder rescatar mas de 2
    tiwtters.

--- SeleniunPratica/.vscode/seleniumPractica.py
+++ SeleniunPratica/.vscode/seleniumPractica.py
@@ -2,30 +2,46 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+numeroTiwtters = 5
+tiwtterNivel = 3
+elementosNuevos = False
+listaTiwtter = []
+
 driver = webdriver.Chrome(executable_path=r"D:\DriverChrome\chromedriver.exe")
+driver.maximize_window()
 driver.get("https://twitter.com/AnaPao_LePew")
 
-#//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]
-# driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]').click()
-# time.sleep(5)
-#name session[username_or_email]
-# driver.find_element_by_name('session[username_or_email]').send_keys('EduardoSex307')
-#name session[password]
-# driver.find_element_by_name('session[password]').send_keys('pumas2018')
-#//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div/div
-# driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div/div').click()
-# time.sleep(5)
-#article
-# articulos = driver.find_elements_by_tag_name("article")
 #//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[2]/section/div/div/div[1]/div/div/article
 #//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[2]/section/div/div/div[3]/div/div/article
 #//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[2]/section/div/div/div[4]/div/div/article
 
+time.sleep(3)
+
+for i in range(numeroTiwtters):
+    while elementosNuevos is not True:
+        try:
+            articulo = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[2]/section/div/div/div['+str(tiwtterNivel)+']/div/div/article')
+            elementosNuevos = True
+        except :
+            driver.execute_script("window.scrollTo(0, 500)") 
+    
+    elementosNuevos = False
+
+    textoPartido = articulo.text.split('\n')
+    listaTiwtter+=[textoPartido]
+    tiwtterNivel+=1
+
+time.sleep(3)
+driver.close()
+
+print("Se obtuvieron un total de:"+str(len(listaTiwtter)))
+print("Los cuales son:")
+print(listaTiwtter)
+
 # for articulo in articulos:
-#     # print(articulo.text)
+#     print(articulo.text)
 #     textoPartido = articulo.text.split('\n')
 #     #Tweet fijado
 #     if 'Tweet fijado' not in textoPartido[0]:
 #         print(textoPartido)
         
-driver.close()
