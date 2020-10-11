a797d609f5dd64379aafc312729ac83472d282a1
Rafael Eduardo <remendezp@televisa.com.mx>
2020-10-11 11:48:37 -0500

    Obtención de "n" tiwtters por medio "aticuls"
    Se termina de afinar la o´btención de lot Tiwtters de un usurio, esto
    con la cantidad de setada en una varible local y dejando que el programa
    recupere la mayor cantidad de "articuls" hasta completar dicha cantidad
    o superarla, depués solo rescatar aquellos tiwtters que no contengan la
    leyenda "Tiwtter Fijado".

--- SeleniunPratica/.vscode/seleniumPractica.py
+++ SeleniunPratica/.vscode/seleniumPractica.py
@@ -2,31 +2,39 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+numeroTiwtters = 5
+tiwttersAgragados = 0
+elementosNuevos = False
+listaTiwtter = []
+
 driver = webdriver.Chrome(executable_path=r"D:\DriverChrome\chromedriver.exe")
+driver.maximize_window()
 driver.get("https://twitter.com/AnaPao_LePew")
 
-#//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]
-# driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/a[2]').click()
-# time.sleep(5)
-#name session[username_or_email]
-# driver.find_element_by_name('session[username_or_email]').send_keys('EduardoSex307')
-#name session[password]
-# driver.find_element_by_name('session[password]').send_keys('pumas2018')
-#//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div/div
-# driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[3]/div/div').click()
 time.sleep(5)
 #article
-articulos = driver.find_elements_by_tag_name("article")
-#//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[2]/section/div/div/div[1]/div/div/article
-#//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[2]/section/div/div/div[3]/div/div/article
-#//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div/div[2]/section/div/div/div[4]/div/div/article
+while tiwttersAgragados < numeroTiwtters:
+    articulos = driver.find_elements_by_tag_name("article")
 
-for articulo in articulos:
-    # print(articulo.text)
-    textoPartido = articulo.text.split('\n')
-    #Tweet fijado
-    if 'Tweet fijado' not in textoPartido[0]:
-        print(textoPartido)
+    # if tiwttersAgragados < numeroTiwtters:
+    #     driver.execute_script("window.scrollTo(0, 500)") 
+    
+    if len(articulos)<numeroTiwtters+1:
+        driver.execute_script("window.scrollTo(0, 500)") 
+    else:
+        for articulo in articulos:
+            # print(articulo.text)
+            textoPartido = articulo.text.split('\n')
+            #Tweet fijado
+            if 'Tweet fijado' not in textoPartido[0]:
+                if tiwttersAgragados < numeroTiwtters:
+                    tiwttersAgragados+=1
+                    listaTiwtter +=[textoPartido]
+                else:
+                    break
+    
+for tiwtter in listaTiwtter:
+    print(tiwtter)
         
 driver.close()
 # https://www.google.com/ 
